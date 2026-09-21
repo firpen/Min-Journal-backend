046d7eedb04f6031a36c72d4363355fc1c099c01
firpen <firrpen@hotmail.com>
2026-09-21 18:42:17 +0200

    setup of the project with spring initializr

---
+++ src/main/java/com/minjournal/min_journal_backend/MinJournalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.minjournal.min_journal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinJournalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinJournalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minjournal/min_journal_backend/MinJournalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjournal.min_journal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinJournalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
